e15329e81ab2f8b0725f3d4cbcae72e976137237
Max Plumer <maxplumer12@gmail.com>
2021-08-16 13:03:57 -0400

    fizzbuzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,20 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+    for (let i=1; i <= n; i++) {
+        // Is the number a multiple of both 3 and 5?
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log('fizzbuzz');
+        // is the number a multiple of 3?
+        } else if (i % 3 === 0) {
+            console.log('fizz');
+        } else if (i % 5 === 0) {
+            console.log('buzz');
+        } else {
+            console.log(i);
+        }
+    }
+}
 
 module.exports = fizzBuzz;
