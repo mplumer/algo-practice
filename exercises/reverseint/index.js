34d3b0efff61f2308cfdcb921a0dbc281d599cd1
Max Plumer <maxplumer12@gmail.com>
2021-08-15 22:46:26 -0400

    reverse int

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,14 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+   const reversed = n.toString().split('').reverse().join('');
 
+//    if (n < 0 ) {
+//        return parseInt(reversed) * -1;
+//    }
+
+   return parseInt(reversed) * Math.sign(n); //Math.sign returns 1 if positive or -1 if negative
+}
+    
 module.exports = reverseInt;
