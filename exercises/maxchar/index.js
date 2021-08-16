07de17f0f1074fc4554619d29f6fd70cd53eebb9
Max Plumer <maxplumer12@gmail.com>
2021-08-15 23:10:03 -0400

    find maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,31 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// iterate through string and for every character either add charactter as a property with value of 1.
+// if character has been seen add 1.
+
+function maxChar(str) {
+    const charMap  = {};
+    let max = 0;
+    let maxChar = '';
+
+    for (let char of str) {
+        if (charMap[char]) {
+            charMap[char]++;
+        } else {   
+            charMap[char] = 1;
+        }
+    }
+
+    for (let char in charMap) {
+        if (charMap[char] > max) {
+            max = charMap[char];
+            maxChar = char;
+        }
+    }
+
+    return maxChar;
+}
+    
 
 module.exports = maxChar;
