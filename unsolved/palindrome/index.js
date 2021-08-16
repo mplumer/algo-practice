38008cc1f64332d5b7bd3b366862ea91a1515e42
Max Plumer <maxplumer12@gmail.com>
2021-08-16 15:24:30 -0400

    palindromes

--- unsolved/palindrome/index.js
+++ unsolved/palindrome/index.js
@@ -7,6 +7,20 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    return str.split('').every((char, i) => {
+        return char === str[str.length -i -1];
+    }); 
+    
+}
 
 module.exports = palindrome;
+
+
+// Solution 1
+
+// function palindrome(str) {
+//     let reversed = str.split('').reverse().join('');
+
+//     return str === reversed;
+// }
